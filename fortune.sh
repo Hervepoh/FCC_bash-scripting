81a194cb5e651e0f73397f4da5f544a907ec61b8
Ngando Epoh Herve <epohherve63@gmail.com>
2023-05-04 14:11:04 +0100

    Update fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -11,3 +11,26 @@
 ###################################################################################
 
 echo -e "\n~~ Fortune Teller ~~\n"
+
+# Create a function
+GET_FORTUNE(){
+  echo "Ask a yes or no question:"
+  read QUESTION
+}
+
+# Do until the question end with an ?
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+done
+
+
+# create an array
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+# random number betwen 0 and 5
+N=$(( RANDOM % 6 )) 
+
+
+echo ${RESPONSES[$N]}
+
+
